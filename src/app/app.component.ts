135a481db2e8aa32b5a25280a5b404f712431129
Mostapha Mahdavi <Mostapha.Mahdavi@gmail.com>
2020-09-08 22:42:27 +0430

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  messages: Array<string> = [];
+
   title = 'ChatHubClient';
+  messageTxt: string;
+  hubConnection: HubConnection;
+
+  ngOnInit() {
+
+    this.hubConnection = new HubConnectionBuilder()
+      .withUrl("https://localhost:44342/chatHub")
+      .build();
+
+
+    this.hubConnection.on("ReceiveMessage", (message: string) => {
+      this.messages.push(message);
+
+    });
+
+    this.hubConnection.start().catch((e) => {
+      console.log(e);
+    });
+
+  }
+
+  sendMessageToHub() {
+       this.hubConnection.send("SendMessage", this.messageTxt);
+       this.messageTxt='';
+  }
 }
